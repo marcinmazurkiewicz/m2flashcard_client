bd877b1b06d069b8e89a29dd4c9f97b0345aebb3
Marcin Mazurkiewicz <marcin@mazurkiewicz.sh>
2020-09-24 09:58:33 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app-wrapper">
+    <Nav />
+    
+  </div>
+</template>
+
+<script>
+  import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  }
+}
+</script>
+
+<style>
+#app-wrapper {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.top-nav {
+  width: 100%;
+  text-align: left;
+  padding: 24px;
+  border-bottom: #2c3e50 solid;
+}
+
+.btn {
+  margin: 12px;
+}
+
+.login-buttons {
+  margin-left:900px;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+      <div class="container p-2 menu-top">
+            <nav class="navbar navbar-light center-divs">
+              <div class="row">
+                <div class="col-8">
+                  <a href="/">M2Flashcards</a>
+                </div>
+              </div>
+                <ul class="navbar nav">
+                  <li class="nav-item">
+                    <router-link :to="{name: 'OProjekcie'}" exact  class="menu-item"><button type="button" class="btn btn-primary">Zaloguj się</button></router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link :to="{name: 'Kontakt'}" exact class="menu-item"><button type="button" class="btn btn-success">Zrejestruj się</button></router-link>
+                  </li>
+                </ul>
+              <!-- Collapsible content -->
+            </nav>
+            <!--/.Navbar-->
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+}
+</script>
+
+<style>
+    .menu-item  {
+        text-decoration: none;
+    }
+    
+</style>
