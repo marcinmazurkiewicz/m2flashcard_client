4e35af6983f7b4cf064fbe279a58d0b9fbcbc029
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-10-01 12:16:27 +0200

    Error handling added

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="form-wrapper">
+      <div class="alert alert-success" role="alert" v-if="registerSuccessfull">
+        Rejestracja zakończona pomyślnie!
+      </div>
         <form>
             <div class="form-group">
-                <label for="userEmail">Adres email</label>
-                <input v-model="userEmail" type="email" class="form-control" id="userEmail">
+                <label for="email">Adres email</label>
+                <input v-model="email" type="email" class="form-control" id="email">
+                <div class="alert alert-danger" role="alert" v-if="errors.email">
+                  {{ $t(getErrorMsg(errors.email.errorType)) }}
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label for="password">Hasło</label>
+                <input v-model="password" type="password" class="form-control" id="password">
+                <div class="alert alert-danger" role="alert" v-if="errors.password">
+                  {{ $t(getErrorMsg(errors.password.errorType)) }}
+                </div>
             </div>
             <div class="form-group">
-                <label for="userPassword">Hasło</label>
-                <input v-model="userPassword" type="password" class="form-control" id="userPassword">
-            </div>
-            <div class="form-group">
-                <label for="userPasswordConfirm">Potwierdź hasło</label>
-                <input v-model="userPasswordConfirm" type="password" class="form-control" id="userPasswordConfirm">
+                <label for="confirmPassword">Potwierdź hasło</label>
+                <input v-model="confirmPassword" type="password" class="form-control" id="confirmPassword">
+                <div class="alert alert-danger" role="alert" v-if="errors.confirmPassword">
+                  {{ $t(getErrorMsg(errors.confirmPassword.errorType)) }}
+                </div>
             </div>
           
             <button v-on:click="sendRegisterRequest()" type="button" class="btn btn-primary">Zarejestruj</button>
@@ -21,29 +34,37 @@
 
 <script>
 import axios from 'axios'
+var componentKey = 'dev.mazurkiewicz.m2flashcards.client.register';
 
 export default {
   name: 'Register',
   data() {
     return {
-      userEmail: "",
-      userPassword: "",
-      userPasswordConfirm: ""
+      email: "",
+      password: "",
+      confirmPassword: "",
+      errors: [],
+      registerSuccessfull: '',
     };
   },
   methods: {
-      sendRegisterRequest() {
-          axios.post('http://localhost:8080/api/v1/users/', {
-              email: this.userEmail,
-              password: this.userPassword,
-              confirmPassword: this.userPasswordConfirm
-          }).then((response) => {
-              console.log(response);
-          }).catch((e) => {
-            console.log(e);
-        });
+    sendRegisterRequest() {
+      axios.post('http://localhost:8080/api/v1/users/', {
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      }).then((response) => {
+        this.errors = [];
+        this.registerSuccessfull = response.status == 201;
+      }).catch((e) => {
+        this.errors = e.response.data.errors;
+      });
+    },
+    getErrorMsg(errorType) {
+      return componentKey + '.' + errorType;
     }
   }
+  
 }
 </script>
 
